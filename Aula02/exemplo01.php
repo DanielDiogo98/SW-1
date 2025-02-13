a60d435696061592e460fc86e83883e8e0079893
Daniel DIogo <159474740+DanielDiogo98@users.noreply.github.com>
2025-02-13 08:55:01 -0300

    Add files via upload

---
+++ Aula02/exemplo01.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 01</title>
+</head>
+<body>
+    <p>Hello World</p>
+
+    <?php
+        $nome="daniel";
+    ?>
+
+    <h1>AULA 02 INTRODUTORIA</h1>
+    <h2>SALVE MUNDO</h2>
+    <?php
+        echo"Hello world";
+        echo"<p>oi eu sou o $nome</p>";
+        echo"<p>oi eu sou o " . $nome . "</p>";
+    ?>
+
+    
+</body>
+</html>
---
+++ Aula02/exemplo02.php
@@ -0,0 +1,8 @@
+<?php
+    $a + 4;
+    $b = "dalmatas 101";
+    $c = $a + $b;
+
+
+    echo $c
+?>
