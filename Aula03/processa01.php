169d2f0b25cd2412d1f378aad45afcb96db6b661
Daniel DIogo <159474740+DanielDiogo98@users.noreply.github.com>
2025-02-27 08:54:59 -0300

    Add files via upload

---
+++ Aula03/processa01.php
@@ -0,0 +1,8 @@
+<?php
+    $nome = $_POST['cxnome'];
+    $idade =  $_POST['cxidade'];
+
+    echo "O seu nome é; $nome <br>";
+    echo "A sua idade  é; $idade<br>";
+
+?>
---
+++ Aula03/processa_exe01.php
@@ -0,0 +1,8 @@
+<?php
+    $nome = $_POST['cxnome'];
+    $msg =  $_POST['cxmsg'];
+
+    echo "O seu nome é; $nome <br>";
+    echo "A sua mensagem é; $msg<br>";
+
+?>
